badb52841a0f1bcedb1332fdfa650a128fc6c91d
emmanuel <eolaojo@yahoo.com>
2016-06-03 16:34:03 -0400

    Added documentation. Restructured some code.

--- src/CSS_Parser/Parser.java
+++ src/CSS_Parser/Parser.java
@@ -2,6 +2,7 @@ package CSS_Parser;
 
 import Exceptions.InvalidExtensionException;
 import Exceptions.InvalidSyntaxException;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -10,6 +11,12 @@ import java.io.FileReader;
 import java.util.*;
 
 /**
+ * CSS parser for Casper. Parses the given CSS file and stores
+ * it's selectors/identifiers and values in a Map. This map
+ * will be passed into the constructor when the object is created
+ * and will be used to determine if multiple CSS style objects
+ * are linked to the same element, classname or id.
+ *
  * @author Emmanuel Olaojo
  * @since 6/1/16
  */
@@ -22,6 +29,18 @@ public class Parser {
     private boolean inComment = false;
     private final InvalidSyntaxException IVSE;
 
+    /**
+     * Constructor takes a file name and the Map that stores the
+     * parsed objects. The extension of the provided file must be css.
+     * This is the first validity check carried out on the file. After
+     * confirming the extension, we attempt to open the file and initialize
+     * the object's properties.
+     *
+     * @param fileName The CSS file to be parsed
+     * @param globMap The Map that stores the parsed objects
+     *
+     * @throws InvalidExtensionException if the file has an Invalid extension.
+     */
     public Parser(String fileName, Map<String, List<CSS>> globMap) throws InvalidExtensionException {
         if(!validExt(fileName)) throw new InvalidExtensionException(fileName);
 
@@ -37,31 +56,60 @@ public class Parser {
         this.IVSE = new InvalidSyntaxException(this.fileName);
     }
 
+    /**
+     * Checks the files extension for validity. The only valid
+     * extension is .css
+     *
+     * @param fileName the filename to check
+     *
+     * @return true if the extension id valid. false otherwise.
+     */
     private boolean validExt(String fileName){
         String[] splitName = fileName.split("\\.");
 
         return splitName[splitName.length-1].toLowerCase().equals("css");
     }
 
+    /**
+     * Reads the file line by line, passes each line through a method
+     * that strips out comments, then it adds the line to a StringBuilder
+     * (StringBuilder is used, over concatenation, for efficiency). If
+     * inComment is true, it checks the current line for the close
+     * and if a close comment is present, the line gets added to the
+     * StringBuilder, else, the line is seen as a comment and is
+     * ignored. When all the lines have been added to the StringBuilder,
+     * we run it through the parseCSS method for further processing.
+     *
+     * @throws InvalidSyntaxException from the parseCSS method
+     */
     public void parse() throws InvalidSyntaxException{
         StringBuilder cssString = new StringBuilder();
 
         this.br.lines().forEach(line -> {
-            if(!this.inComment) {
+            if(!this.inComment || line.contains(CLOSE_COMMENT)) {
                 line = this.stripComments(line.trim());
 
                 if (line.length() > 0) {
                     cssString.append(line);
                 }
             }
-            else{
-                this.inComment = !line.contains(CLOSE_COMMENT);
-            }
         });
 
         this.parseCSS(cssString);
     }
 
+    /**
+     * Handles the parsing of the StringBuilder. It gets the
+     * positions of the next opening and closing curly braces and
+     * checks that they are correctly placed i.e. the index of the opening
+     * curly is less than the index of the closing curly. Then it gets
+     * the next css style block and passes it to parseCssBlock;
+     *
+     * @param cssString The StringBuilder containing trimmed, uncommented css
+     *
+     * @throws InvalidSyntaxException if there's a problem with the braces or
+     * from the methods called (getBlock and parseCssBlock)
+     */
     private void parseCSS(StringBuilder cssString) throws InvalidSyntaxException {
         int openIndex;
         int closeIndex;
@@ -82,6 +130,22 @@ public class Parser {
         }
     }
 
+    /**
+     * This method takes in a css block e.g.
+     *
+     *      .class{color: red;height: 60px;}
+     *
+     * and parses it into a CSS object, then places it in a map with
+     * the associated base selector (see next method) as the key.
+     * Things outside the braces are identifiers and inside the
+     * braces, you have the style. The rest of the process is
+     * pretty straightforward from there just #ReadTheCode
+     *
+     * @param cssBlock a css String
+     *
+     * @throws InvalidSyntaxException if illegal certain Illegal characters
+     * are encountered or from the methods called (createCSS and getBaseSelector);
+     */
     private void parseCssBlock(String cssBlock) throws InvalidSyntaxException {
         String[] styleParts = cssBlock.split("\\{|}");
         String identifiers = styleParts[0];
@@ -89,6 +153,8 @@ public class Parser {
         CSS css = createCSS(style);
         String[] iSplit = identifiers.split(",");
         ArrayList<CSS> styleList;
+
+        if(identifiers.contains(";")) throw this.IVSE;
 
         for(String s: iSplit){
             String key = this.getBaseSelector(s);
@@ -107,6 +173,23 @@ public class Parser {
 //        System.out.println(identifiers + css);
     }
 
+    /**
+     * This method takes in a String of identifiers e.g.
+     *
+     *      .class1 #id1.hello > #world
+     *
+     * and returns the base selector.
+     *
+     * The base selector is the class, element or id that's ultimately
+     * affected by the style.
+     * In the example case, the base selector will be #world because it's the
+     * only one being directly affected by the style.
+     *
+     * @param identifier a String of identifiers
+     *
+     * @throws InvalidSyntaxException if illegal certain Illegal characters
+     * are encountered or from the methods called (createCSS and getBaseSelector);
+     */
     private String getBaseSelector(String identifier) throws InvalidSyntaxException {
         identifier = identifier.trim();
         int start = identifier.length() - 1;
@@ -211,7 +294,10 @@ public class Parser {
         boolean hasStart = startIndex >= 0;
         boolean hasEnd = endIndex >= 0;
 
-        if(!(hasStart || hasEnd)) return line;
+        if(!(hasStart || hasEnd)){
+//            this.inComment
+            return line;
+        }
 
         endIndex = hasEnd ? endIndex: line.length() - 1;
         startIndex = hasStart ? startIndex : 0;
@@ -240,7 +326,7 @@ public class Parser {
         long timeTaken;
 
         try {
-            testParser = new Parser("bootstrap.css", globMap);
+            testParser = new Parser("style2.css", globMap);
             start =  System.currentTimeMillis();
             testParser.parse();
             end = System.currentTimeMillis();
